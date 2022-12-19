8d1496d23070fd7b2cd9970ba00b298412b36256
SamirMamede <anuarsamir@gmail.com>
2022-12-19 16:28:45 -0300

    Setting up our models and database, Task List Page.

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
-def tasklist(request):
-    return HttpResponse('To Do list')
+class TaskList(ListView):
+    model = Task
